fe9f665f7288a45e08a0aecb0033292dc7b8abaa
Darkfirs <144917076+Darkfirs@users.noreply.github.com>
2023-09-20 15:54:25 +0300

    hh

--- homework/Homework 1/Homework 1 Task 1 Abramov.py
+++
@@ -1,8 +0,0 @@
-def findres(x):
-    res = (x**2+1)*(x**2+x)+1
-    ans = f'{x}^4+{x}^3+x^{2}+x+1 = {res}'
-    return ans
-
-if __name__ == '__main__':
-    x = int(input('''Введите значение х для формулы:'''))
-    print(findres(x))

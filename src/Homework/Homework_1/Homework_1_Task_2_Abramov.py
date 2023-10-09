eefb252fdfebc8ebf33a93faddde7f7cbc29ef7b
Darkfirs <144917076+Darkfirs@users.noreply.github.com>
2023-10-09 14:41:25 +0300

    update1

--- src/Homework/Homework_1/Homework_1_Task_2_Abramov.py
+++ src/Homework/Homework_1/Homework_1_Task_2_Abramov.py
@@ -1,62 +1,76 @@
 import math as m
 
-def scalar_product(a,b):
+
+def scalar_product(a, b):
     res = 0
     for i in range(len(a)):
         res += a[i] * b[i]
     return res
+
+
 def length(a):
     sum = 0
     for i in range(len(a)):
         if a[i] != 0:
             sum += a[i] ** 2
-    res = sum ** 0.5
+    res = sum**0.5
     return res
 
 
 def mat1():
-    n,m = int(input('Введите количество строк:')), int(input('Введите количество столбцов:'))
-    print('Вводите через пробел числа строки:')
+    n, m = int(input("Введите количество строк:")), int(
+        input("Введите количество столбцов:")
+    )
+    print("Вводите через пробел числа строки:")
     matrix = []
     for i in range(n):
-        matrix.append(list(map(int,input().split())))
-    tra_matrix =[[0 for j in range(len(matrix))] for i in range(len(matrix[0]))]
+        matrix.append(list(map(int, input().split())))
+    tra_matrix = [[0 for j in range(len(matrix))] for i in range(len(matrix[0]))]
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             tra_matrix[j][i] = matrix[i][j]
     for i in range(len(tra_matrix)):
         for j in range(len(tra_matrix[i])):
-            print(tra_matrix[i][j], end=' ')
+            print(tra_matrix[i][j], end=" ")
         print()
 
 
-
 def mat2():
-    n, m = int(input('Введите количество строк для 1-й матрицы:')), int(input('Введите количество столбцов для 1-й матрицы:'))
-    print('Вводите через пробел числа строки для 1-й матрицы:')
-    matrix_1 = []
-    for i in range(n):
-        matrix_1.append(list(map(int,input().split())))
-    a,b = int(input('Введите количество строк для 2-й матрицы:')), int(input('Введите количество столбцов для 2-й матрицы:'))
-    print('Вводите через пробел числа строки для 2-й матрицы:')
-    matrix_2 = []
-    for i in range(a):
-        matrix_2.append(list(map(int,input().split())))
-
-    res_mat = [[matrix_1[i][j] + matrix_2[i][j] for j in range
-    (len(matrix_1[0]))] for i in range(len(matrix_1))]
-    for x in res_mat:
-        print(x)
-def mat3():
-    n, m = int(input('Введите количество строк для 1-й матрицы:')), int(
-        input('Введите количество столбцов для 1-й матрицы:'))
-    print('Вводите через пробел числа строки для 1-й матрицы:')
+    n, m = int(input("Введите количество строк для 1-й матрицы:")), int(
+        input("Введите количество столбцов для 1-й матрицы:")
+    )
+    print("Вводите через пробел числа строки для 1-й матрицы:")
     matrix_1 = []
     for i in range(n):
         matrix_1.append(list(map(int, input().split())))
-    a, b = int(input('Введите количество строк для 2-й матрицы:')), int(
-        input('Введите количество столбцов для 2-й матрицы:'))
-    print('Вводите через пробел числа строки для 2-й матрицы:')
+    a, b = int(input("Введите количество строк для 2-й матрицы:")), int(
+        input("Введите количество столбцов для 2-й матрицы:")
+    )
+    print("Вводите через пробел числа строки для 2-й матрицы:")
+    matrix_2 = []
+    for i in range(a):
+        matrix_2.append(list(map(int, input().split())))
+
+    res_mat = [
+        [matrix_1[i][j] + matrix_2[i][j] for j in range(len(matrix_1[0]))]
+        for i in range(len(matrix_1))
+    ]
+    for x in res_mat:
+        print(x)
+
+
+def mat3():
+    n, m = int(input("Введите количество строк для 1-й матрицы:")), int(
+        input("Введите количество столбцов для 1-й матрицы:")
+    )
+    print("Вводите через пробел числа строки для 1-й матрицы:")
+    matrix_1 = []
+    for i in range(n):
+        matrix_1.append(list(map(int, input().split())))
+    a, b = int(input("Введите количество строк для 2-й матрицы:")), int(
+        input("Введите количество столбцов для 2-й матрицы:")
+    )
+    print("Вводите через пробел числа строки для 2-й матрицы:")
     matrix_2 = []
     for i in range(a):
         matrix_2.append(list(map(int, input().split())))
@@ -65,15 +79,21 @@ def mat3():
     for i in range(len(matrix_1)):
         for j in range(len(matrix_2[0])):
             for k in range(len(matrix_2)):
-                matrix_void[i][j] +=(matrix_1[i][k] * matrix_2[k][j])
+                matrix_void[i][j] += matrix_1[i][k] * matrix_2[k][j]
     for x in matrix_void:
         print(x)
+
+
 def matsp():
-    choice = int(input('''Что именно вы хотите сделать?
+    choice = int(
+        input(
+            """Что именно вы хотите сделать?
         1.Транспонировать
         2.Сложить
         3.Найти произведение
-        :'''))
+        :"""
+        )
+    )
     if choice == 1:
         mat1()
     if choice == 2:
@@ -81,32 +101,39 @@ def matsp():
     if choice == 3:
         mat3()
 
+
 def vek1():
-    vek_a = list(map(int,input('Введите значения вектора a через пробел: ').split()))
-    vek_b = list(map(int,input('Введите значения вектора b через пробел: ').split()))
-    if len(vek_b)!= len(vek_a):
-        print('Ошибка, размерность векторов не совпадает')
+    vek_a = list(map(int, input("Введите значения вектора a через пробел: ").split()))
+    vek_b = list(map(int, input("Введите значения вектора b через пробел: ").split()))
+    if len(vek_b) != len(vek_a):
+        print("Ошибка, размерность векторов не совпадает")
     else:
-        print('Результат скалярного произведения равен: ',scalar_product(vek_a,vek_b))
+        print("Результат скалярного произведения равен: ", scalar_product(vek_a, vek_b))
 
 
 def vek2():
-    vek_a = list(map(int, input('Введите значения вектора a через пробел: ').split()))
-    print('Длинная вектора а равна', length(vek_a))
-def vek3():
-    vek_a = list(map(int, input('Введите значения вектора a через пробел: ').split()))
-    vek_b = list(map(int, input('Введите значения вектора b через пробел: ').split()))
-    rr = scalar_product(vek_a,vek_b)/round(length(vek_a)*length(vek_b))
+    vek_a = list(map(int, input("Введите значения вектора a через пробел: ").split()))
+    print("Длинная вектора а равна", length(vek_a))
 
-    print(f'Угол между векторами примерно равен:',round(m.degrees(m.acos(rr))))
+
+def vek3():
+    vek_a = list(map(int, input("Введите значения вектора a через пробел: ").split()))
+    vek_b = list(map(int, input("Введите значения вектора b через пробел: ").split()))
+    rr = scalar_product(vek_a, vek_b) / round(length(vek_a) * length(vek_b))
+
+    print(f"Угол между векторами примерно равен:", round(m.degrees(m.acos(rr))))
 
 
 def veksp():
-    choice = int(input('''Что именно вы хотите сделать?
+    choice = int(
+        input(
+            """Что именно вы хотите сделать?
     1.Найти скалярное произведение
     2.Вычислить длину
     3.Найти угол между ними
-    :'''))
+    :"""
+        )
+    )
     if choice == 1:
         vek1()
     if choice == 2:
@@ -116,11 +143,16 @@ def veksp():
 
 
 def start():
-    choice = int(input('Вы хотите работать с векторами(1) или матрицами(2)(введите только число): '))
+    choice = int(
+        input(
+            "Вы хотите работать с векторами(1) или матрицами(2)(введите только число): "
+        )
+    )
     if choice == 1:
         veksp()
     if choice == 2:
         matsp()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     start()
--- src/Practice/Practice_1/Practice 1 Task 1 Abramov.py
+++ src/Practice/Practice_1/Practice 1 Task 1 Abramov.py
@@ -1,4 +1,4 @@
-def res(a,b):
+def res(a, b):
     c = 0
     n = 1
     while True:
@@ -8,15 +8,16 @@ def res(a,b):
             c = n
         n += 1
     q = a - b * c
-    return c,q
+    return c, q
+
 
 def sear():
-    a = int(input('Введите число а(делимое):'))
-    b = int(input('Введите число b(делитель):'))
+    a = int(input("Введите число а(делимое):"))
+    b = int(input("Введите число b(делитель):"))
 
-    print(f'Результат деления и остаток {res(a,b)}')
+    print(f"Результат деления и остаток {res(a,b)}")
     # r=a-b*q
 
-if __name__ == '__main__':
-    sear()
 
+if __name__ == "__main__":
+    sear()

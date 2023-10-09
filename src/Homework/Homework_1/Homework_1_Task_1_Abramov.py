48f07acbc52270bd3a2a705432ef02b21a06a59e
Darkfirs <144917076+Darkfirs@users.noreply.github.com>
2023-10-09 14:27:06 +0300

    Update hm1t1

--- src/Homework/Homework_1/Homework_1_Task_1_Abramov.py
+++ src/Homework/Homework_1/Homework_1_Task_1_Abramov.py
@@ -3,6 +3,7 @@ def findres(x):
     res = (z + 1) * (z + x) + 1
     ans = f"{x}^4+{x}^3+x^{2}+x+1 = {res}"
     return ans
+    
 
 if __name__ == "__main__":
     x = int(input("""Введите значение х для формулы:"""))

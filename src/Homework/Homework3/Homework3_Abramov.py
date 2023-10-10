ecfbc318c61fd52a848a05f0c2020990137b7fb1
Darkfirs <144917076+Darkfirs@users.noreply.github.com>
2023-10-10 19:49:19 +0300

    hm3_t1

---
+++ src/Homework/Homework3/Homework3_Abramov.py
@@ -0,0 +1,36 @@
+def curry_explicit(function, arity):
+
+    def ma_funct(arguments):
+
+        def curry_function(arg):
+            return ma_funct([*arguments, arg])
+
+        if arity == len(arguments):
+            return function(*arguments)
+        return curry_function
+    if arity < 0:
+        print("Отрицательная арность невозможна")
+    elif arity == 0:
+        return lambda: function()
+    arg_list = []
+    return ma_funct(arg_list)
+
+
+def uncurry_explicit(function, arity):
+    if arity < 0:
+        print("Отрицательная арность невозможна")
+    def uncurry_function(*args):
+        if len(args) == 1 or len(args) == 0:
+            return function(*args)
+        res = function(args[0])
+        for i in range(1, arity):
+            res = res(args[i])
+        return res
+    return uncurry_function
+
+
+if __name__ == "__main__":
+    f2 = curry_explicit((lambda x, y, z: f"<{x},{y},{z}>"), 3)
+    g2 = uncurry_explicit(f2, 3)
+    print(f2(123)(456)(562))
+    print(g2(123, 456, 562))

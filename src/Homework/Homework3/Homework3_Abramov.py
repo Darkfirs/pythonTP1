b1dab4a6e2e217dcc77122b175cb701ddee48143
Darkfirs <144917076+Darkfirs@users.noreply.github.com>
2023-10-17 18:32:25 +0300

    fix exception

--- src/Homework/Homework3/Homework3_Abramov.py
+++ src/Homework/Homework3/Homework3_Abramov.py
@@ -1,27 +1,23 @@
-import sys
-
-
 def curry_explicit(function, arity):
-    def check(arguments):
+    def control(arguments):
         def curry_function(arg):
-            return check([*arguments, arg])
+            return control([*arguments, arg])
 
         if arity == len(arguments):
             return function(*arguments)
         return curry_function
 
     if arity < 0:
-        sys.exit("Отрицательная арность невозможна")
-        return 0
+        raise ValueError("Отрицательная арность невозможна")
     elif arity == 0:
-        return function()
+        return function
     arg_list = []
-    return check(arg_list)
+    return control(arg_list)
 
 
 def uncurry_explicit(function, arity):
     if arity < 0:
-        sys.exit("Отрицательная арность невозможна")
+        raise ValueError("Отрицательная арность невозможна")
 
     def uncurry_function(*args):
         if len(args) == 1 or len(args) == 0:
@@ -37,5 +33,5 @@ def uncurry_explicit(function, arity):
 if __name__ == "__main__":
     f2 = curry_explicit((lambda x, y, z: f"<{x},{y},{z}>"), 3)
     print(f2(123)(456)(562))
-    g2 = uncurry_explicit(f2, -3)
+    g2 = uncurry_explicit(f2, 3)
     print(g2(123, 456, 562))

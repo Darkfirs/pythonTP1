3cef828cdf697a6a9aaba04db9cd1aca35fb718d
Darkfirs <144917076+Darkfirs@users.noreply.github.com>
2023-10-27 14:56:56 +0300

    fix save

--- src/Test/test1/test1_task1.py
+++ src/Test/test1/test1_task1.py
@@ -1,20 +1,20 @@
 import os
 import sys
 
-def check_files(file1,file2):
-    if os.path.isfile(f"{file1}") and not(os.path.isfile(f'{file2}')):
+def check_files(file1, file2):
+    if os.path.isfile(f"{file1}") and not (os.path.isfile(f"{file2}")):
         return True
     elif os.path.isfile(f"{file2}"):
         print("Файл g существует")
         return False
-    elif not(os.path.isfile(f'{file1}')):
+    elif not (os.path.isfile(f"{file1}")):
         print("Нет файла f")
         return False
 
 
-def save_words(file2,dict_words):
-    with open(file2,"w")as file:
-        for i,g in  dict_words.items():
+def save_words(file2, dict_words):
+    with open(file2, "w") as file:
+        for i, g in dict_words.items():
             c = f"{i} = {g} \n"
             file.write(c)
 
@@ -22,13 +22,13 @@ def save_words(file2,dict_words):
 def open_find(file1):
     dict = {}
     all_letter = ""
-    with open(file1,"r")as file:
+    with open(file1, "r") as file:
         for line in file:
             let = line.split()
             for g in range(len(let)):
                 all_letter += let[g]
             for i in range(len(all_letter)):
-                if "A" <=all_letter[i]<= "Z" or "a"<=all_letter[i]<="z" :
+                if "A" <= all_letter[i] <= "Z" or "a" <= all_letter[i] <= "z":
                     if not all_letter[i] in dict:
                         dict[f"{all_letter[i]}"] = 1
                     else:
@@ -38,11 +38,11 @@ def open_find(file1):
     return res
 
 
-def main(f1,f2):
-    if check_files(f1,f2):
-        save_words(f2,open_find(f1))
+def main(f1, f2):
+    if check_files(f1, f2):
+        save_words(f2, open_find(f1))
 
 
 if __name__ == "__main__":
-    file1, file2 = sys.argv[1],sys.argv[2]
-    main(file1,file2)
+    file1, file2 = sys.argv[1], sys.argv[2]
+    main(file1, file2)

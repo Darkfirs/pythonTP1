80472c8c570ff5fee3adf5b9d93ff5c8423179ce
Darkfirs <144917076+Darkfirs@users.noreply.github.com>
2023-11-24 16:11:07 +0300

    del useless if

--- src/Test/Test2/fibonacci.py
+++ src/Test/Test2/fibonacci.py
@@ -1,7 +1,4 @@
 def fibonacci(n):
-    if n < 0 or n > 90:
-        raise ValueError("n must be between 0 and 90")
-
     a, b = 0, 1
     for _ in range(n):
         a, b = b, a + b
@@ -12,8 +9,7 @@ def check_enter(n):
     if not n.isdigit():
         print("You must enter integer number")
         return False
-    n = int(n)
-    if not (0 <= n <= 90):
+    if not (0 <= int(n) <= 90):
         print("You must enter number between 0 and 90")
         return False
     return True

f39a79001ce7d7859bb4f02c1ed2eccaab0db97c
Darkfirs <144917076+Darkfirs@users.noreply.github.com>
2023-12-01 15:17:01 +0300

    fix

--- src/Test/Test2/spy.py
+++ src/Test/Test2/spy.py
@@ -7,17 +7,23 @@ def logger(log_file):
         @wraps(func)
         def wrapper(*args, **kwargs):
             current_time = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-            log_args = ", ".join(f"{k}={v}" for k, v in zip(func.__code__.co_varnames, args))
+            log_args = ", ".join(
+                f"{k}={v}" for k, v in zip(func.__code__.co_varnames, args)
+            )
             log_args += ", ".join(f"{k}={v}" for k, v in kwargs.items())
 
             try:
                 result = func(*args, **kwargs)
                 with open(log_file, "a") as file:
-                    file.write(f"{current_time} {func.__name__} {log_args} result={result}\n")
+                    file.write(
+                        f"{current_time} {func.__name__} {log_args} result={result}\n"
+                    )
                 return result
             except Exception as e:
                 with open(log_file, "a") as file:
-                    file.write(f"{current_time} {func.__name__} {log_args} error={str(e)}\n")
+                    file.write(
+                        f"{current_time} {func.__name__} {log_args} error={str(e)}\n"
+                    )
                 raise e
 
         return wrapper
@@ -25,12 +31,12 @@ def logger(log_file):
     return decorator
 
 
-@logger('my_log.txt')
+@logger("my_log.txt")
 def f(a, b):
     if a != 0:
         return f(a - 1, b - 1)
     return b
 
 
-f(2,1)
-f(b=6,a=3)
+f(2, 1)
+f(b=6, a=3)

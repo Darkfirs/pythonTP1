f127635015b1156b8a180787628b6db028128901
Darkfirs <144917076+Darkfirs@users.noreply.github.com>
2023-12-22 21:47:09 +0300

    Abramov test2 task2

---
+++ src/Test/Test2/safecall.py
@@ -0,0 +1,40 @@
+import traceback
+import warnings
+from functools import wraps
+
+
+def safe_call(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            error_info = {
+                "function": func.__name__,
+                "exception_type": type(e).__name__,
+                "exception_message": str(e),
+                "traceback": traceback.format_exc()
+            }
+            warning_message = (
+                f"Warning:\n"
+                f"Function: {error_info['function']}\n"
+                f"Type of Exception: {error_info['exception_type']}\n"
+                f"Message: {error_info['exception_message']}\n"
+                f"In line {error_info['traceback'].splitlines()[-2].lstrip('  ')}\n"
+            )
+            warnings.warn(warning_message, category=UserWarning)
+            return None
+    return wrapper
+
+
+@safe_call
+def example_function(dividend, divisor):
+    result = dividend / divisor
+    return result
+
+
+result = example_function(10, 2)
+print("Result:", result)
+
+result = example_function(10, "b")
+print("Result:", result)
---
+++ tests/test/tests_test2/test_safecall.py
@@ -0,0 +1,71 @@
+from src.Test.Test2.safecall import safe_call
+import pytest
+import warnings
+
+
+@safe_call
+def ex_funct_1(a, b):
+    return a+b
+
+
+@safe_call
+def ex_funct_2(a, b):
+    return a / b
+
+
+@pytest.mark.parametrize(
+    "num_1, num_2, expected",
+    [
+        (5, 10, 15),
+        (20, 10, 30),
+        (1, 2, 3)
+    ],
+)
+def test_work_functions_correct(num_1, num_2, expected):
+    assert ex_funct_1(num_1, num_2) == expected
+
+
+@pytest.mark.parametrize(
+    "num_1, num_2, expected",
+    [
+        (2, 0, None)
+    ],
+)
+def test_work_functions_error(num_1, num_2, expected, capsys):
+    with capsys.disabled():
+        with warnings.catch_warnings(record=True) as w:
+            result = ex_funct_2(num_1, num_2)
+            assert result == expected
+
+            assert len(w) == 1
+            warning = w[0].message
+            assert str(warning) == (
+                f"Warning:\n"
+                f"Function: ex_funct_2\n"
+                f"Type of Exception: ZeroDivisionError\n"
+                f"Message: division by zero\n"
+                f"In line return a / b\n"
+            )
+
+
+@pytest.mark.parametrize(
+    "num_1, num_2, expected",
+    [
+        (2, "b", None)
+    ],
+)
+def test_work_functions_error_strtyps(num_1, num_2, expected, capsys):
+    with capsys.disabled():
+        with warnings.catch_warnings(record=True) as w:
+            result = ex_funct_2(num_1, num_2)
+            assert result == expected
+
+            assert len(w) == 1
+            warning = w[0].message
+            assert str(warning) == (
+                f"Warning:\n"
+                f"Function: ex_funct_2\n"
+                f"Type of Exception: TypeError\n"
+                f"Message: unsupported operand type(s) for /: 'int' and 'str'\n"
+                f"In line return a / b\n"
+            )
